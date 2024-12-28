ce75441abb12a1d465152206fd238c5a68ff991a
bruh <cardboardboxpepe@gmail.com>
2024-12-28 02:40:13 -0700

    added some checks to ensure that challenges have their proper flags i.e., stack canaries, etc.

--- pwn/.scripts/regenerate.sh
+++ pwn/.scripts/regenerate.sh
@@ -4,6 +4,36 @@ source ./.scripts/constants.sh
 # This automagically updates & configures the challenges' Makefiles.
 # Dockerfiles, and .gitignores
 
+function check_canary {
+    # setup
+    which readelf 1>/dev/null 2>/dev/null
+    if [[ $? -ne 0 ]]; then
+        echo "readelf isn't on the path, cannot verify stack canaries."
+    fi
+
+    # check for stack canary function
+    if readelf -s $1 2> /dev/null | grep " UND " | grep -Eq "__stack_chk_fail|__stack_chk_guard"; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+function check_pie {
+    # setup
+    which readelf 1>/dev/null 2>/dev/null
+    if [[ $? -ne 0 ]]; then
+        echo "readelf isn't on the path, cannot verify stack canaries."
+    fi
+
+    # check for stack canary function
+    if readelf -h $1 2> /dev/null | grep "Type:" | grep -q "DYN"; then
+        return 1
+    else
+        return 0
+    fi
+}
+
 for chal in $CHALS; do
     if [[ $chal != *"$1"* ]]; then
         continue
@@ -22,5 +52,15 @@ for chal in $CHALS; do
     cd "$chal/src"
     make clean
     make debug
-    cd -
+    cd - 1>&2 2>/dev/null
 done
+
+# for stack2
+check_canary stack2
+if [[ $? -ne 0 ]]; then
+    echo "stack2 has a canary, this is incorrect."
+fi
+check_pie stack2
+if [[ $? -ne 0 ]]; then
+    echo "stack2 has a canary, this is incorrect."
+fi
