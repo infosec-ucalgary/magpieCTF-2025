614b2b05df5ff4b583d2f44411777b4f5fbc5561
bruh <cardboardboxpepe@gmail.com>
2025-01-16 13:46:55 -0700

    need to completely redo stack2

--- pwn/stack2/src/chal.c
+++ pwn/stack2/src/chal.c
@@ -6,108 +6,36 @@
 #define FLAG_SIZE 128
 #define BUFFER_SIZE 0x100
 
-#define LENGTH 16
-#define DEFAULT_USERNAME "Christina"
-#define DEFAULT_PASSWORD "Crypto"
+#define ERR_CHALLENGE_FAILURE 2
+#define ERR_NO_MALLOC 3
+#define ERR_OTHER 4
 
-typedef struct _user {
-    char username[LENGTH];
-    char password[LENGTH];
-    int admin;
-} user_t;
-
-int login(user_t *cmp) {
-    char local_user[LENGTH];
-    char local_pass[LENGTH];
-
-    printf("Username: ");
-    fgets(local_user, LENGTH - 1, stdin);
-
-    printf("Password: ");
-    fgets(local_pass, LENGTH - 1, stdin);
-
-    // compare the two
-    if (strcmp(local_user, cmp->username) != 0) {
-        return 0;
-    }
-    if (strcmp(local_pass, cmp->password) != 0) {
-        return 0;
-    }
-
-    return 1;
-}
-
-void menu() {
-    puts("-- NYPD Terminal v2 --");
-    puts("1. Change username");
-    puts("2. Change password");
-    puts("3. Exit");
-    printf("> ");
-}
-
-void change_username(user_t *cmp) {
-    char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
-    if (buffer == NULL) {
-        puts("Failed to allocate memory for buffer, cannot proceed.");
-        exit(-2);
-    }
-
-    printf("Enter new username: ");
-    fgets(buffer, BUFFER_SIZE - 1, stdin);
-    strcpy(cmp->username, buffer); // vulnerable!
-    free(buffer);
-}
-
-void change_password(user_t *cmp) {
-    char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
-    if (buffer == NULL) {
-        puts("Failed to allocate memory for buffer, cannot proceed.");
-        exit(-2);
-    }
-
-    printf("Enter new password: ");
-    fgets(buffer, BUFFER_SIZE - 1, stdin);
-    strcpy(cmp->password, buffer); // vulnerable!
-    free(buffer);
-}
+void menu() {}
 
 void win() {
+    // insert impossible condition here
+
+    // flag buffer
+    char flag_buffer[FLAG_SIZE];
+
     // open the file
     FILE *fd = fopen(FLAG, "r");
-    if (fd == 0) {
+    if (fd < 0) {
         puts("Flag cannot be found, contact the CTF organizers.");
-        exit(1);
-    }
-
-    char *buffer = malloc(sizeof(char) * FLAG_SIZE);
-    if (buffer == NULL) {
-        puts("Failed to allocate memory for buffer, cannot proceed.");
-        exit(-2);
+        exit(ERR_OTHER);
     }
 
     // read flag into buffer
-    fgets(buffer, FLAG_SIZE - 1, fd);
+    fgets(flag_buffer, FLAG_SIZE - 1, fd);
     fclose(fd);
-    printf("Only you can be trusted with this... %s\n", buffer);
+    printf("Only you can be trusted with this... %s\n", flag_buffer);
+    exit(0);
 }
 
 int main(int argc, char **argv) {
     // setup io
     setvbuf(stdin, NULL, _IONBF, 0);
     setvbuf(stdout, NULL, _IONBF, 0);
-
-    // setup user
-    user_t user = {
-        .username = DEFAULT_USERNAME,
-        .password = DEFAULT_PASSWORD,
-        .admin = 0
-    };
-
-    // login
-    if (!login(&user)) {
-        puts("Intruder!");
-        exit(1);
-    }
 
     // main loop
     while (1) {
@@ -118,20 +46,7 @@ int main(int argc, char **argv) {
 
         // get choice
         fscanf(stdin, "%d", &option);
-
-        // perform choice
-        switch (option) {
-        case 1: // change username
-            change_username(&user);
-            break;
-        case 2: // change password
-            change_password(&user);
-            break;
-        case 3: // exit
-            goto LOOP_EXIT; // terrible goto statement
-        default:
-            break;
-        }
+        getchar();
     }
 
 LOOP_EXIT:
