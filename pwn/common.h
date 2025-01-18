c580e8abb00fb4afc0f9e426e201543006d1f316
Liam Major <liam.major91@gmail.com>
2025-01-18 15:04:32 -0700

    added a header file to contain common commands

---
+++ pwn/common.h
@@ -0,0 +1,11 @@
+#ifndef __COMMON_PWN_CTF__
+#define __COMMON_PWN_CTF__ 1
+
+#define ERR_CHALLENGE_FAILURE 2
+#define ERR_NO_MALLOC 3
+#define ERR_OTHER 4
+
+#define FLAG "flag.txt"
+#define FLAG_SIZE 128
+
+#endif /** __COMMON_PWN_CTF__ */
