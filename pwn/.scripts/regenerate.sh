29d212fc4569447f33571decfd547769c3059067
bruh <cardboardboxpepe@gmail.com>
2025-01-18 17:10:03 -0700

    updated build pipeline

--- pwn/.scripts/regenerate.sh
+++ pwn/.scripts/regenerate.sh
@@ -1,37 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 source ./.scripts/constants.sh
+source ./.scripts/assert.sh
 
 # This automagically updates & configures the challenges' Makefiles.
 # Dockerfiles, and .gitignores
 
-function check_canary {
-    # setup
-    which readelf 1>/dev/null 2>/dev/null
-    if [[ $? -ne 0 ]]; then
-        echo "readelf isn't on the path, cannot verify stack canaries."
-    fi
-
+function check_canary() {
     # check for stack canary function
-    if readelf -s $1 2> /dev/null | grep " UND " | grep -Eq "__stack_chk_fail|__stack_chk_guard"; then
-        return 1
-    else
-        return 0
-    fi
+    readelf -s "$1/src/$1" | grep -Eq "__stack_chk"
+    echo "$?"
 }
 
-function check_pie {
-    # setup
-    which readelf 1>/dev/null 2>/dev/null
-    if [[ $? -ne 0 ]]; then
-        echo "readelf isn't on the path, cannot verify stack canaries."
-    fi
-
+function check_pie() {
     # check for stack canary function
-    if readelf -h $1 2> /dev/null | grep "Type:" | grep -q "DYN"; then
-        return 1
-    else
-        return 0
-    fi
+    readelf -h "$1/src/$1" 2>/dev/null | grep "Type:" | grep -q "DYN"
+    echo "$?"
 }
 
 for chal in $CHALS; do
@@ -54,3 +37,26 @@ for chal in $CHALS; do
     make debug
     cd - 1>&2 2>/dev/null
 done
+
+# setup
+which readelf 1>/dev/null 2>/dev/null
+if [[ $? -ne 0 ]]; then
+    echo "readelf isn't on the path, cannot verify stack canaries."
+    exit 1
+fi
+
+# checking challenges for canaries
+assert_eq $(check_canary printf1) 0 "printf1 should have a canary"
+assert_eq $(check_canary printf2) 0 "printf2 should have a canary"
+assert_not_eq $(check_canary overflow1) 0 "overflow1 shouldn't have a canary"
+assert_not_eq $(check_canary overflow2) 0 "overflow2 shouldn't have a canary"
+assert_not_eq $(check_canary ret2libc1) 0 "ret2libc1 shouldn't have a canary"
+assert_eq $(check_canary ret2libc2) 0 "ret2libc2 should have a canary"
+
+# checking aslr
+assert_eq $(check_pie printf1) 0 "printf1 should have ASLR"
+assert_eq $(check_pie printf2) 0 "printf2 should have ASLR"
+assert_not_eq $(check_pie overflow1) 0 "overflow1 shouldn't have ASLR"
+assert_eq $(check_pie overflow2) 0 "overflow2 should have ASLR"
+assert_not_eq $(check_pie ret2libc1) 0 "ret2libc1 shouldn't have ASLR"
+assert_eq $(check_pie ret2libc2) 0 "ret2libc2 should have ASLR"
