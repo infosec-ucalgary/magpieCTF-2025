8f6b8ba352bbca39230589dc894146b3e61af91c
bruh <cardboardboxpepe@gmail.com>
2025-01-22 00:58:38 -0700

    final addition to the build pipeline

--- pwn/.scripts/build.sh
+++ pwn/.scripts/build.sh
@@ -4,20 +4,39 @@ source ./.scripts/constants.sh
 CWD=$(pwd)
 IMAGES=0
 PROGS=0
+CHECK=1
 
 # setting opts
 case "$1" in
-image*)
+im*)
     IMAGES=1
     ;;
-prog*)
+pr*)
     PROGS=1
+    ;;
+ch*)
+    CHECK=1
     ;;
 *)
     IMAGES=1
     PROGS=1
+    CHECK=1
     ;;
 esac
+
+# checking for nsjail
+docker image ls | grep nsjail
+if [[ $? -ne 0 ]]; then
+    # pulling and building nsjail
+    if [[ ! -d "./nsjail" ]]; then
+        echo "Docker image 'nsjailcontainer' wasn't found, pulling from github."
+        git clone https://github.com/google/nsjail.git nsjail
+    fi
+
+    # building the image
+    cd nsjail
+    docker build -t nsjailcontainer .
+fi
 
 # building images
 if [ $IMAGES -eq 1 ]; then
@@ -58,4 +77,9 @@ if [ $PROGS -eq 1 ]; then
 
     # clean up
     rm -rf "/tmp/image.tar"
+fi
+
+# checking the challenge flags
+if [ $CHECK -eq 1 ]; then
+    check_chals
 fi
--- pwn/.scripts/constants.sh
+++ pwn/.scripts/constants.sh
@@ -6,13 +6,13 @@ TAGROOT="magpiesctf2025/pwn"
 
 function check_canary() {
     # check for stack canary function
-    readelf -s "$1/src/$1" | grep -Eq "__stack_chk"
+    readelf -s "$1" | grep -Eq "__stack_chk"
     echo "$?"
 }
 
 function check_pie() {
     # check for stack canary function
-    readelf -h "$1/src/$1" 2>/dev/null | grep "Type:" | grep -q "DYN"
+    readelf -h "$1" 2>/dev/null | grep "Type:" | grep -q "DYN"
     echo "$?"
 }
 
@@ -25,19 +25,19 @@ function check_chals() {
     fi
     
     # checking challenges for canaries
-    assert_eq $(check_canary printf1) 0 "printf1 should have a canary"
-    assert_eq $(check_canary printf2) 0 "printf2 should have a canary"
-    assert_not_eq $(check_canary overflow1) 0 "overflow1 shouldn't have a canary"
-    assert_not_eq $(check_canary overflow2) 0 "overflow2 shouldn't have a canary"
-    assert_not_eq $(check_canary ret2libc1) 0 "ret2libc1 shouldn't have a canary"
-    assert_eq $(check_canary ret2libc2) 0 "ret2libc2 should have a canary"
+    assert_eq $(check_canary dist/printf1) 0 "printf1 should have a canary"
+    assert_eq $(check_canary dist/printf2) 0 "printf2 should have a canary"
+    assert_not_eq $(check_canary dist/overflow1) 0 "overflow1 shouldn't have a canary"
+    assert_not_eq $(check_canary dist/overflow2) 0 "overflow2 shouldn't have a canary"
+    assert_not_eq $(check_canary dist/ret2libc1) 0 "ret2libc1 shouldn't have a canary"
+    assert_eq $(check_canary dist/ret2libc2) 0 "ret2libc2 should have a canary"
     
     # checking aslr
-    assert_eq $(check_pie printf1) 0 "printf1 should have ASLR"
-    assert_eq $(check_pie printf2) 0 "printf2 should have ASLR"
-    assert_not_eq $(check_pie overflow1) 0 "overflow1 shouldn't have ASLR"
-    assert_eq $(check_pie overflow2) 0 "overflow2 should have ASLR"
-    assert_eq $(check_pie ret2libc1) 0 "ret2libc1 should have ASLR"
-    assert_eq $(check_pie ret2libc2) 0 "ret2libc2 should have ASLR"
+    assert_eq $(check_pie dist/printf1) 0 "printf1 should have ASLR"
+    assert_eq $(check_pie dist/printf2) 0 "printf2 should have ASLR"
+    assert_not_eq $(check_pie dist/overflow1) 0 "overflow1 shouldn't have ASLR"
+    assert_eq $(check_pie dist/overflow2) 0 "overflow2 should have ASLR"
+    assert_eq $(check_pie dist/ret2libc1) 0 "ret2libc1 should have ASLR"
+    assert_eq $(check_pie dist/ret2libc2) 0 "ret2libc2 should have ASLR"
 }
 
