9894b5bc06031c24566e1d855398848b5a0b7542
Liam Major <liam.major91@gmail.com>
2025-01-22 00:38:43 -0700

    updated build pipeline, now builds challenge within a docker container

--- pwn/.scripts/build.sh
+++ pwn/.scripts/build.sh
@@ -7,16 +7,16 @@ PROGS=0
 
 # setting opts
 case "$1" in
-    image*)
-        IMAGES=1
-        ;;
-    prog*)
-        PROGS=1
-        ;;
-    *)
-        IMAGES=1
-        PROGS=1
-        ;;
+image*)
+    IMAGES=1
+    ;;
+prog*)
+    PROGS=1
+    ;;
+*)
+    IMAGES=1
+    PROGS=1
+    ;;
 esac
 
 # building images
@@ -27,24 +27,34 @@ if [ $IMAGES -eq 1 ]; then
         cd "$CWD/$chal/src"
 
         # building the intermediate image with all the build shit
-        docker build . -t "$TAGROOT/$chal:build" --target build
+        docker build . -t "$TAGROOT-$chal:build" --target build
 
         # building the production image
-        docker build . -t "$TAGROOT/$chal:latest"
+        docker build . -t "$TAGROOT-$chal:latest"
     done
     cd $CWD
 fi
 
 # building progs (requires containers be built)
 if [ $PROGS -eq 1 ]; then
+    mkdir -p "$CWD/dist/"
     for chal in $CHALS; do
-        echo "Compiling $chal"
-        cd "$CWD/$chal/src"
-        make clean
-        make
+        # checking if image exists
+        docker image ls | grep "$TAGROOT-$chal" | grep build 1>&2 2>/dev/null
+        if [[ $? -ne 0 ]]; then
+            echo "Cannot extract $chal from $TAGROOT-$chal:build, because it does not exist."
+            continue
+        fi
+
+        # exporting the image to a tar archive
+        docker export "$(docker create "$TAGROOT-$chal:build")" --output /tmp/image.tar
+
+        # copying out the challenges
+        tar -xvf /tmp/image.tar -C "$CWD/dist/" "ctf/$chal" --strip-components=1
+        tar -xvf /tmp/image.tar -C "$CWD/$chal/src/" "ctf/$chal.debug" --strip-components=1
     done
     cd $CWD
 
-    # checking the compilation
-    check_chals
+    # clean up
+    rm -rf "/tmp/image.tar"
 fi
