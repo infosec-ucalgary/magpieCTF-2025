f6c51063db8498b692e0ca12e17512ae956236b2
bruh <cardboardboxpepe@gmail.com>
2025-02-01 15:33:50 -0700

    changed flags for copying

--- pwn/scripts/regenerate.sh
+++ pwn/scripts/regenerate.sh
@@ -15,11 +15,11 @@ TARGETS=("$CHALS")
 
 for chal in $TARGETS; do
     # updating the docker file
-    cp -f ./base.Dockerfile "$CWD/$chal/src/Dockerfile"
+    cp -vu ./base.Dockerfile "$CWD/$chal/src/Dockerfile"
     sed -i "s/BINARY_NAME/$chal/g" "$CWD/$chal/src/Dockerfile"
 
     # updating the makefile
-    cp -f ./base.mk "$CWD/$chal/src/Makefile"
+    cp -vu ./base.mk "$CWD/$chal/src/Makefile"
     sed -i "s/BINARY_NAME/$chal/g" "$CWD/$chal/src/Makefile"
     
     # update the gitignore
@@ -27,7 +27,7 @@ for chal in $TARGETS; do
     echo "common.h" >> "$CWD/$chal/src/.gitignore"
 
     # copying the header file
-    cp -f "$CWD/common.h" "$CWD/$chal/src/common.h"
+    cp -vu "$CWD/common.h" "$CWD/$chal/src/common.h"
 
     # logging
     echo "Regenerated files for $chal."
