95d495e88673814d1a009886fb2a73b5e120749d
Dante <dkirsman@gmail.com>
2025-02-01 22:26:48 -0700

    feat(lore and theme): add lore and theme for web challange

--- web/jupyter/start.sh
+++ web/jupyter/start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 /usr/sbin/sshd
-
-exec su - hawk -c "jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token=''"
+exec su - rhash -c "cd /home/rhash/notebooks && jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token=''"
