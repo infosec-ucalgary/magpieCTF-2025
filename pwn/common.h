4540d2d2156890ed5ef4133a0006748f14aaccc7
Liam Major <liam.major91@gmail.com>
2025-02-01 21:04:54 -0700

    updated the IPs

--- pwn/common.h
+++ pwn/common.h
@@ -1,5 +1,5 @@
-#ifndef __COMMON_PWN_CTF__
-#define __COMMON_PWN_CTF__ 1
+#ifndef __MAGPIECTF_2025_PWN__
+#define __MAGPIECTF_2025_PWN__ 1
 
 // return codes
 #define ERR_CHALLENGE_FAILURE 2
@@ -16,13 +16,13 @@
 #define TIME_FSTRING "%a %b %d %k:%M:%S %Z 1933"
 
 // for plot
-#define IP_CORS "74.6.9.245"
+#define IP_CORS "10.0.0.254"
 #define IP_EXPLO "84.9.10.5"
 #define IP_HASH "167.85.6.40"
 #define IP_BLUE "89.167.8.45"
-#define IP_JAKE "10.0.0.254"
+#define IP_JAKE "52.129.50.254"
 
 void ssh_login(const char *__hostname, const char *__user, const char *__dst_ip,
                const char *__src_ip);
 
-#endif /** __COMMON_PWN_CTF__ */
+#endif /** __MAGPIECTF_2025_PWN__ */
