07a0469709d4b504367e20f9fc2cd1b1bed1a641
Ayeshas18 <146018840+Ayeshas18@users.noreply.github.com>
2025-02-08 21:23:53 -0700

    Delete forensics/forensics_easy/src/create.ps1

--- forensics/forensics_easy/src/create.ps1
+++
@@ -1,5 +0,0 @@
-echo "magpieCTF{this_is_the_flag}" > flag.txt
-zip -e -9 flag.zip flag.txt Form.pdf
-steghide embed -cf wallpaper.jpg -ef flag.zip
-exiftool -Comment="Password for zip : bXlfc2VjcmV0X3Bhc3N3b3Jk" wallpaper.jpg
-rm wallpaper.jpg_original
