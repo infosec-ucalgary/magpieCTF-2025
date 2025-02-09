c52722634564414aadf02716c7cd1f306a83a39a
Ayeshas18 <146018840+Ayeshas18@users.noreply.github.com>
2025-02-08 21:24:12 -0700

    Add files via upload

---
+++ forensics/forensics_easy/src/create.sh
@@ -0,0 +1,7 @@
+echo "magpieCTF{this_is_the_flag}" > flag.txt
+zip -e -9 flag.zip flag.txt Form.pdf
+my_secret_password
+my_secret_password
+steghide embed -cf wallpaper.jpg -ef flag.zip
+exiftool -Comment="Password for zip : bXlfc2VjcmV0X3Bhc3N3b3Jk" wallpaper.jpg
+
