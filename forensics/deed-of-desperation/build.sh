706336ca9e1f903f58ec5190efa1fbd43eb301ec
Liam Major <liam.major91@gmail.com>
2025-02-18 17:22:50 -0700

    update(overhauled build script)

--- forensics/deed-of-desperation/build.sh
+++ forensics/deed-of-desperation/build.sh
@@ -1,5 +1,21 @@
-zip -e -9 flag.zip src/flag.txt src/Form.pdf
-F0rS4l3!2025
-steghide embed -cf wallpaper.jpg -ef flag.zip
-exiftool -Comment="Password for zip : RjByUzRsMyEyMDI1" wallpaper.jpg
+TARGET_ZIP=flag.zip
+STEG_PASSWORD=""
+ZIP_PASSWORD=F0rS4l3!2025
+ENCRYPTED=$(base64 <<<"$ZIP_PASSWORD")
+SRC_IMAGE=wallpaper.jpg
 
+# creating the dist dir
+mkdir -vp ./dist ./build
+
+# zipping everything together
+zip -P "$ZIP_PASSWORD" -9 build/flag.zip src/flag.txt src/Form.pdf
+
+# embedding the
+steghide --embed -v -e none \
+    -p "$STEG_PASSWORD" \
+    --stegofile "dist/$SRC_IMAGE" \
+    --coverfile "src/$SRC_IMAGE" \
+    --embedfile build/flag.zip
+
+# adding the base64 encoded password as a comment
+exiftool -Comment="$ENCRYPTED" "dist/$SRC_IMAGE"
