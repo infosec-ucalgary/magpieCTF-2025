60d2f842bbaa116e9fa4b363596851d99729d1ac
bruh <cardboardboxpepe@gmail.com>
2025-02-20 01:04:29 -0700

    redo solve script

---
+++ web/cookie-trail/solve/solve.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# remote host
+CTF_HOST="localhost"
+
+# request to the server
+RESPONSE=$(curl -v -s -b "name=10" http://$CTF_HOST/check)
+
+# checking the response
+echo "$RESPONSE" | grep -q "magpieCTF"
+if [ $? -eq 0 ]; then
+    echo "MagpieCTF - cookie-trail : True"
+else
+    echo "MagpieCTF - cookie-trail : Frue"
+fi
