66204460592d876286a8a5a6c50e9f273dc6674c
bruh <cardboardboxpepe@gmail.com>
2025-02-19 17:10:24 -0700

    fix(now encrypts dynamically and actually has the key inside)

--- web/cops_like_ciphers_and_cookies/src/app.js
+++ web/cops_like_ciphers_and_cookies/src/app.js
@@ -1,36 +1,135 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
-var path = require('path');
+const cookieParser = require("cookie-parser");
+var path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+// -- vigenere cipher --
+// all the vigenere functions operate in lower case
+
+/**
+ * This function creates a cyclical key for vigenere encrypton.
+ * @param {string} str The string to be encrypted.
+ * @param {string} key The key of the cipher.
+ * @returns A key of greater or equal length to str.
+ */
+function vigenere_key(str, key) {
+  // generating a key to fit the length of str
+  parts = [];
+  for (let i = 0; i < Math.ceil(str.length / key.length); ++i) {
+    parts.push(key.toLowerCase());
+  }
+  return parts.join("");
+}
+
+/**
+ * Encrypts data using a vigenere cipher
+ * @param {string} plaintext The text to be encrypted
+ * @param {string} key The key to encrypt with
+ * @returns A vigenere cipher encoded string.
+ */
+function vigenere_encrypt(plaintext, key) {
+  let cipher_text = "";
+  let _str = plaintext.toLowerCase();
+  let _key = vigenere_key(_str, key);
+  let _base = "a".charCodeAt(0);
+
+  for (let i = 0; i < _str.length; i++) {
+    // converting in range 0-25
+    let x =
+      (_str[i].charCodeAt(0) - _base + (_key[i].charCodeAt(0) - _base)) % 26;
+
+    // convert into alphabets(ASCII)
+    console.log(`${_str[i]} + ${_key[i]} = ${String.fromCharCode(x + _base)}`);
+    cipher_text += String.fromCharCode(x + _base);
+  }
+  return cipher_text;
+}
+
+/**
+ * Decrypts data using a vigenere cipher
+ * @param {string} ciphertext The text to decrypt
+ * @param {string} key The key to decrypt with
+ * @returns A plaintext string.
+ */
+function vigenere_decrypt(ciphertext, key) {
+  let orig_text = "";
+  let _str = ciphertext.toLowerCase();
+  let _key = vigenere_key(_str, key);
+  let _base = "a".charCodeAt(0);
+
+  for (let i = 0; i < _str.length; i++) {
+    // converting in range 0-25
+    let x =
+      (_str[i].charCodeAt(0) - _base + (_key[i].charCodeAt(0) - _base)) % 26;
+
+    // convert into alphabets(ASCII)
+    console.log(`${_str[i]} + ${_key[i]} = ${String.fromCharCode(x + _base)}`);
+    orig_text += String.fromCharCode(x + _base);
+  }
+  return orig_text;
+}
+
+// -- server code --
+
+// encryption related
+const key = "consi";
+const user = "user";
+const admin_user = "admin";
+const guest_user = "guest";
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
-app.get('/', (req, res)=>{
-    res.cookie('wgrj', 'iirkb', { 
-        httpOnly: true, 
-        secure: true, // set to true if using HTTPS
-        sameSite: 'lax'
-    });
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+app.get("/", (req, res) => {
+  // creating the cookie
+  let _key = vigenere_encrypt(user, key);
+  let _value = vigenere_encrypt(guest_user, key);
+
+  // logging
+  console.log(
+    `Created cookie ${_key} = ${_value} for ${
+      req.headers["x-forwarded-for"] || req.socket.remoteAddress
+    }`
+  );
+
+  // setting the cookie
+  res.cookie(_key, _value, {
+    httpOnly: true,
+    secure: true, // set to true if using HTTPS
+    sameSite: "lax",
+  });
+  res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
-app.get('/hidden', (req, res)=>{
-    const user = req.cookies.wgrj
-    if (user != 'crzav'){
-        res.status(200);
-        res.sendFile(path.join(__dirname, 'public', 'hidden.html'));
-    } else {
-        res.status(200);
-        res.sendFile(path.join(__dirname, 'public', 'flag.html'));
-    }
+// adding routes
+app.get("/login", (req, res) => {
+  let _user = req.cookies[vigenere_encrypt(user, key)];
+  res.status(200);
+
+  // logging
+  console.log(`Attempted login: cookies.user = ${_user}`);
+
+  // testing
+  if (_user === vigenere_encrypt(admin_user, key)) {
+    // hacker cracked the cipher
+    console.log(
+      `Hacker cracked the cipher: cookies.${vigenere_encrypt(
+        user,
+        key
+      )}(${user}) = ${vigenere_encrypt(admin_user, key)}(${admin_user})`
+    );
+    res.sendFile(path.join(__dirname, "public", "flag.html"));
+  } else {
+    // hacker failed or first visit
+    res.sendFile(path.join(__dirname, "public", "hidden.html"));
+  }
 });
 
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
