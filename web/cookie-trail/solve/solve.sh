afbdeabd6091a489f02926e159e0b57e06b6f262
Liam Major <liam.major91@gmail.com>
2025-02-20 01:36:00 -0700

    updated solve scripts

--- web/cookie-trail/solve/solve.sh
+++ web/cookie-trail/solve/solve.sh
@@ -1,15 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# remote host
-CTF_HOST="localhost"
+# checking arguments
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <hostname> <port>"
+    exit 1
+fi
+
+# vars
+HOST="$1"
+PORT="$2"
 
 # request to the server
-RESPONSE=$(curl -s -b "name=10" http://$CTF_HOST/check)
+RESPONSE=$(curl -s -b "name=10" http://$HOST:$PORT/check)
 
 # checking the response
 echo "$RESPONSE" | grep -q "magpieCTF"
 if [ $? -eq 0 ]; then
     echo "MagpieCTF - cookie-trail : True"
+    exit 0
 else
-    echo "MagpieCTF - cookie-trail : Frue"
+    echo "MagpieCTF - cookie-trail : False"
+    exit 1
 fi
