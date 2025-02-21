45d4dae179803bf5c84db416858842f7c44cc64a
Christina <rijiaohe@gmail.com>
2025-02-20 22:28:34 -0500

    recreated header to fit the scheme

--- web/HeaderWebEasy/README.md
+++ web/HeaderWebEasy/README.md
@@ -4,13 +4,14 @@ Author: *Royce*
 
 >Difficulty: \<easy>
 
-Flag: `magpieCTF{__HttP_h34dS_Upp}`
+Flag: `magpieCTF{I_Don't_Und3rstand_Whats_In_H3r_SHt+p_H3ad}`
 
 ## Backstory
-Suspect Explo apparently has a secret website hosted at [website here], but it doesnt look like there’s anything wrong with it upon first glance. Once an HTTP head request is sent, the website changes to a hitpiece rant about how Explo hates christina and how christina betrayed him.
+Suspect Explo apparently has a secret website hosted at [website here], but it doesnt look like there’s anything wrong with it upon first glance. 
 
 ## Intended Solve
 To complete this challenge, the users need to simply send an HTTP HEAD request to the server. This can be done using tools like Insomnia or Postman.  
+
 
 ## Handouts
 - [Website Address Here]
--- web/HeaderWebEasy/src/app.py
+++ web/HeaderWebEasy/src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 IMAGE_PATH = 'image.jpg'
 
-FLAG = "magpie{Ht+p_H3ad!!}"
+FLAG = "magpieCTF{I_Don't_Und3rstand_Whats_In_H3r_SHt+p_H3ad}"
 
 @app.route('/', methods=['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'])
 def handle_requests():
@@ -15,7 +15,7 @@ def handle_requests():
         return '', 200, {'FLAG': FLAG}
 
     elif request.method == 'POST':
-        return jsonify({"message": "I aint missing my post requests..."}), 200
+        return jsonify({"message": "POST my wants here, nobody will find out the secret"}), 200
 
     elif request.method == 'PUT':
         # Return a custom message for PUT
@@ -23,7 +23,7 @@ def handle_requests():
     
     else:
         # Return a default message for other methods
-        return jsonify({"message": "Sorry bud, your exotic HTTP methods are not welcome here."}), 405
+        return jsonify({"message": "Those METHOD of yours looks much like hers. I'm so sick of it"}), 405
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
